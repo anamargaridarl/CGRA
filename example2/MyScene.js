d2f5d774455afeec56edc922cd60526b93a30e04
Unknown <anamargaridarl@gmail.com>
2019-03-08 12:50:12 +0000

    added madeira

--- example2/MyScene.js
+++ example2/MyScene.js
@@ -26,11 +26,11 @@ class MyScene extends CGFscene {
         this.cone = new MyCone(this, 3, 1);
         this.pyramid = new MyPyramid(this, 3, 1);
         this.cube = new MyCube(this);
-        
+
         this.objects = [this.plane, this.pyramid, this.cone, this.cube];
 
         // Labels and ID's for object selection on MyInterface
-        this.objectIDs = { 'Plane': 0 , 'Pyramid': 1, 'Cone': 2, 'Cube': 3};
+        this.objectIDs = { 'Plane': 0, 'Pyramid': 1, 'Cone': 2, 'Cube': 3 };
 
         //Other variables connected to MyInterface
         this.selectedObject = 0;
@@ -39,14 +39,14 @@ class MyScene extends CGFscene {
         this.displayNormals = false;
         this.objectComplexity = 0.5;
         this.scaleFactor = 2.0;
-   
+
 
     }
     initLights() {
 
         this.GlobalLight = 0.3;
-       
-        this.setGlobalAmbientLight(this.GlobalLight,this.GlobalLight, this.GlobalLight, 1.0);
+
+        this.setGlobalAmbientLight(this.GlobalLight, this.GlobalLight, this.GlobalLight, 1.0);
 
         this.lights[0].setPosition(2.0, 2.0, -1.0, 1.0);
         this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
@@ -66,23 +66,22 @@ class MyScene extends CGFscene {
         this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(10, 10, 10), vec3.fromValues(0, 0, 0));
     }
 
-    hexToRgbA(hex)
-    {
+    hexToRgbA(hex) {
         var ret;
         //either we receive a html/css color or a RGB vector
-        if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
-            ret=[
-                parseInt(hex.substring(1,3),16).toPrecision()/255.0,
-                parseInt(hex.substring(3,5),16).toPrecision()/255.0,
-                parseInt(hex.substring(5,7),16).toPrecision()/255.0,
+        if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+            ret = [
+                parseInt(hex.substring(1, 3), 16).toPrecision() / 255.0,
+                parseInt(hex.substring(3, 5), 16).toPrecision() / 255.0,
+                parseInt(hex.substring(5, 7), 16).toPrecision() / 255.0,
                 1.0
             ];
         }
         else
-            ret=[
-                hex[0].toPrecision()/255.0,
-                hex[1].toPrecision()/255.0,
-                hex[2].toPrecision()/255.0,
+            ret = [
+                hex[0].toPrecision() / 255.0,
+                hex[1].toPrecision() / 255.0,
+                hex[2].toPrecision() / 255.0,
                 1.0
             ];
         return ret;
@@ -99,12 +98,12 @@ class MyScene extends CGFscene {
 
     };
 
-    updateObjectComplexity(){
+    updateObjectComplexity() {
         this.objects[this.selectedObject].updateBuffers(this.objectComplexity);
     }
 
-    updateAmbientLight(){
-        this.setGlobalAmbientLight(this.GlobalLight,this.GlobalLight,this.GlobalLight,1.0);
+    updateAmbientLight() {
+        this.setGlobalAmbientLight(this.GlobalLight, this.GlobalLight, this.GlobalLight, 1.0);
     }
 
     initMaterials() {
@@ -129,6 +128,7 @@ class MyScene extends CGFscene {
         this.material3.setSpecular(1, 0, 0, 1.0);
         this.material3.setShininess(10.0);
 
+
         // Custom material (can be changed in the interface)
         // initially midrange values on ambient, diffuse and specular, on R, G and B respectively
 
@@ -142,10 +142,17 @@ class MyScene extends CGFscene {
 
         this.updateCustomMaterial();
 
-        this.materials = [this.material1, this.material2, this.material3, this.customMaterial];
+        //Madeira
+        this.material4 = new CGFappearance(this);
+        this.material4.setAmbient(0.37, 0.2,0.09, 1.0);
+        this.material4.setDiffuse(0.37, 0.2, 0.09, 1.0);
+        this.material4.setSpecular(0.03125, 0.0125, 0.006, 0.01);
+        this.material4.setShininess(10.0);
+
+        this.materials = [this.material1, this.material2, this.material3, this.material4, this.customMaterial];
 
         // Labels and ID's for object selection on MyInterface
-        this.materialIDs = {'Red Ambient': 0, 'Red Diffuse': 1, 'Red Specular': 2, 'Custom': 3 };
+        this.materialIDs = { 'Red Ambient': 0, 'Red Diffuse': 1, 'Red Specular': 2, 'Madeira': 3, 'Custom': 4 };
     }
     display() {
         // ---- BEGIN Background, camera and axis setup
@@ -157,7 +164,7 @@ class MyScene extends CGFscene {
         this.loadIdentity();
         // Apply transformations corresponding to the camera position relative to the origin
         this.applyViewMatrix();
-        
+
         this.lights[0].update();
         this.lights[1].update();
 
@@ -170,14 +177,14 @@ class MyScene extends CGFscene {
         this.materials[this.selectedMaterial].apply();
 
         this.pushMatrix();
-        this.scale(this.scaleFactor,this.scaleFactor,this.scaleFactor);
-        
-        
+        this.scale(this.scaleFactor, this.scaleFactor, this.scaleFactor);
+
+
         if (this.displayNormals)
             this.objects[this.selectedObject].enableNormalViz();
         else
             this.objects[this.selectedObject].disableNormalViz();
-        
+
         this.objects[this.selectedObject].display();
         this.popMatrix();
         // ---- END Primitive drawing section
