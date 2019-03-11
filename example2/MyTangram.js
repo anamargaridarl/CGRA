f983f16b261d8aa021c7c1dca360cd13056771fc
Gonçalo Oliveira <goncalo.oli@outlook.com>
2019-03-11 20:24:52 +0000

    Reset custom material on diamond

--- example2/MyTangram.js
+++ example2/MyTangram.js
@@ -4,7 +4,6 @@ class MyTangram extends CGFobject {
 
         this.current_scene = scene;
 
-<<<<<<< HEAD
         this.diamond = new MyDiamond(scene);
         this.triangle = new MyTriangle(scene);
         this.parallelogram = new MyParallelogram(scene);
@@ -44,18 +43,8 @@ class MyTangram extends CGFobject {
 
         this.material_big_triangle2 = new CGFappearance(scene)
         this.material_big_triangle2.setSpecular(1, 1, 1, 1)
-        this.material_big_triangle2.setDiffuse(0, 0, 1, 1)
-        this.material_big_triangle2.setAmbient(0, 0, 1, 1)
-=======
-        this.diamond = new MyDiamond(this.current_scene);
-        this.triangle = new MyTriangle(this.current_scene);
-        this.parallelogram = new MyParallelogram(this.current_scene);
-        this.smallTriangle = new MyTriangleSmall(this.current_scene);
-        this.bigTriangle = new MyTriangleBig(this.current_scene);
-    
-
-        // this.initBuffers()
->>>>>>> fd7a44bde5db7143e8cd9ab7bc29e933f735c340
+        this.material_big_triangle2.setDiffuse(0, 155/255, 1, 1)
+        this.material_big_triangle2.setAmbient(0, 155/255, 1, 1)
     }
 
     display() {
@@ -69,13 +58,8 @@ class MyTangram extends CGFobject {
             0.0, 1, 0.0, 0.0,
             0.0, 0.0, 1, 0.0,
             -Math.cos(Math.PI / 4), 2 + Math.sqrt(2) / 2, 0.0, 1.0];
-<<<<<<< HEAD
             
-        this.material_diamond.apply()
-=======
-
-    
->>>>>>> fd7a44bde5db7143e8cd9ab7bc29e933f735c340
+        this.current_scene.customMaterial.apply()
         this.current_scene.pushMatrix();
         this.current_scene.multMatrix(tDiam);
         this.current_scene.multMatrix(rDiam);
@@ -89,10 +73,7 @@ class MyTangram extends CGFobject {
         this.triangle.display();
         this.current_scene.popMatrix();
         
-<<<<<<< HEAD
         this.material_small_triangle.apply()
-=======
->>>>>>> fd7a44bde5db7143e8cd9ab7bc29e933f735c340
         this.current_scene.pushMatrix();
         this.current_scene.translate(0, 1, 0);
         this.smallTriangle.display();
@@ -112,23 +93,14 @@ class MyTangram extends CGFobject {
         this.bigTriangle.display();
         this.current_scene.popMatrix();
 
-<<<<<<< HEAD
         this.material_big_triangle2.apply()
-=======
-
->>>>>>> fd7a44bde5db7143e8cd9ab7bc29e933f735c340
         this.current_scene.pushMatrix();
         this.current_scene.translate(Math.sqrt(8) / 2, - (Math.sqrt(8) / 2), 0);
         this.current_scene.rotate(-(Math.PI / 2 + Math.PI / 4), 0, 0, 1);
-        this.bigTriangle.display();
+        this.bigTriangle2.display();
         this.current_scene.popMatrix();
-<<<<<<< HEAD
         
         this.material_parallelogram.apply()
-=======
-
-
->>>>>>> fd7a44bde5db7143e8cd9ab7bc29e933f735c340
         this.current_scene.pushMatrix();
         this.current_scene.translate(-Math.SQRT1_2, 0, 0);
         this.current_scene.scale(-1, 1, 1);
