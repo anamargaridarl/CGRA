30218b6568409e4bb5910d5877f03c820d67533e
Gonçalo Oliveira <goncalo.oli@outlook.com>
2019-03-30 16:31:51 +0000

    Trees in tree patch should now have small random deviations in their positions

--- projectA/MyTreeGroupPatch.js
+++ projectA/MyTreeGroupPatch.js
@@ -10,28 +10,26 @@ class MyTreeGroupPatch extends CGFobject {
 
         for (var i = 0; i < 9; ++i) {
             this.trees.push(new MyTree(scene, 2, 2, 2, 2))
-            this.offsets.push([(Math.trunc((Math.random() * 10)) % 3) - 1, 0, (Math.trunc((Math.random() * 10)) % 3) - 1])
+            this.offsets.push([(Math.trunc((Math.random() * 10)) % 3) - 1, (Math.trunc((Math.random() * 10)) % 3) - 1])
         }
 
         console.log(this.offsets)
     }
 
     display() {
-        for (var i = 0; i < 3; ++i) {
+
+        for (var i = 0; i < 9; i += 3) {
             this.scene.pushMatrix()
-            this.scene.translate(0, 0, j > 0 ? 5 * i: 0);
-            
-            for (var j = 0, tree_index = 3 * i + j; j < 3; ++j) {
-                this.scene.pushMatrix()
+            this.scene.translate(0, 0, 5 * (i / 3))
+            this.trees[i].display()
 
-                this.scene.translate(5 * j, 0, 0);
-                
-                this.trees[tree_index].display();
-                this.scene.popMatrix();
-            }
+            this.scene.translate(5 + this.offsets[i + 1][0], 0, this.offsets[i + 1][1])
+            this.trees[i + 1].display()
 
-            this.scene.popMatrix();
+            this.scene.translate(5 + this.offsets[i + 2][0], 0, this.offsets[i + 2][1])
+            this.trees[i + 2].display()
 
+            this.scene.popMatrix()
         }
     }
 }
