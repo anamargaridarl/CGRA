4240710d468ee2bf82902187f8f5d0a3d1df4a1d
Gonçalo Oliveira <goncalo.oli@outlook.com>
2019-04-07 18:26:07 +0100

    Fixed texCoords in cone

--- projectA/MyCone.js
+++ projectA/MyCone.js
@@ -13,6 +13,7 @@ class MyCone extends CGFobject {
         this.vertices = [];
         this.indices = [];
         this.normals = [];
+        this.texCoords = [];
 
         var ang = 0;
         var alphaAng = 2*Math.PI/this.slices;
@@ -21,10 +22,16 @@ class MyCone extends CGFobject {
             this.vertices.push(Math.cos(ang), 0, -Math.sin(ang));
             this.indices.push(i, (i+1) % this.slices, this.slices);
             this.normals.push(Math.cos(ang), Math.cos(Math.PI/4.0), -Math.sin(ang));
-            this.texCoords.push(0.5, 0, 0, 1, 1, 1);
+            
+            if (i % 2 == 0) {
+                this.texCoords.push(0.5,0,0,1)
+            }
+
             ang+=alphaAng;
         }
         this.vertices.push(0,1,0);
+        this.texCoords.push(1,1);
+        
         this.normals.push(0,1,0);
 
 
