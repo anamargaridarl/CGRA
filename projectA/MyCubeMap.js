3d6a3dca6f7d24d2dc7b70e1304956f1a5d28b6f
Gonçalo Oliveira <goncalo.oli@outlook.com>
2019-04-14 21:59:07 +0100

    Implemented skybox

--- projectA/MyCubeMap.js
+++ projectA/MyCubeMap.js
@@ -2,12 +2,317 @@ class MyCubeMap extends CGFobject {
 	constructor(scene) {
         super(scene);
         
-        this.cubemap = new MyUnitCube(scene);
+        this.cubemap_texture = new CGFappearance(scene)
+        this.cubemap_texture.loadTexture('images/skybox.png')
+
+
+		this.initBuffers();
     }
     
-    display()
-    {
-        this.cubemap.display();
-    }
-	
+	initBuffers() {
+		// this.vertices = [
+		// 	0.5, -0.5, 0.5,		//0
+		// 	-0.5, -0.5, 0.5,	//1
+		// 	-0.5, 0.5, 0.5,		//2
+		// 	0.5, 0.5, 0.5,		//3
+		// 	0.5, -0.5, -0.5,	//4
+		// 	-0.5, -0.5, -0.5,	//5
+		// 	-0.5, 0.5, -0.5,	//6	
+		// 	0.5, 0.5, -0.5,
+		// 	0.5, -0.5, 0.5,		//0
+		// 	-0.5, -0.5, 0.5,	//1
+		// 	-0.5, 0.5, 0.5,		//2
+		// 	0.5, 0.5, 0.5,		//3
+		// 	0.5, -0.5, -0.5,	//4
+		// 	-0.5, -0.5, -0.5,	//5
+		// 	-0.5, 0.5, -0.5,	//6	
+		// 	0.5, 0.5, -0.5,
+		// 	0.5, -0.5, 0.5,		//0
+		// 	-0.5, -0.5, 0.5,	//1
+		// 	-0.5, 0.5, 0.5,		//2
+		// 	0.5, 0.5, 0.5,		//3
+		// 	0.5, -0.5, -0.5,	//4
+		// 	-0.5, -0.5, -0.5,	//5
+		// 	-0.5, 0.5, -0.5,	//6	
+		// 	0.5, 0.5, -0.5    	//7
+		// ];
+
+		// //Counter-clockwise reference of vertices
+		// this.indices = [
+		//    2,3,0,
+		//    2,0,1,
+		//    1,0,4,
+		//    1,4,5,
+		//    0,3,7,
+		//    0,7,4,
+		//    2,1,5,
+		//    2,5,6,
+		//    3,2,6,
+		//    3,6,7,
+		//    7,6,5,
+		//    7,5,4
+           
+		// ];
+
+		// this.normals = [
+		// 	0,0,1,  //0
+		// 	0,0,1,
+		// 	0,0,1,
+		// 	0,0,1,
+		// 	0,0,-1,
+		// 	0,0,-1,
+		// 	0,0,-1,
+		// 	0,0,-1,  //7
+		// 	0,-1,0,  //0
+		// 	0,-1,0,
+		// 	0,1,0,
+		// 	0,1,0,
+		// 	0,-1,0,
+		// 	0,-1,0,
+		// 	0,1,0,
+		// 	0,1,0,   //7
+		// 	1,0,0,
+		// 	-1,0,0,
+		// 	-1,0,0,
+		// 	1,0,0,
+		// 	1,0,0,
+		// 	-1,0,0,
+		// 	-1,0,0,
+		// 	1,0,0
+		// ];
+		
+		// this.primitiveType = this.scene.gl.TRIANGLES;
+		// this.initGLBuffers();
+	}
+
+	initBottom() {
+		this.vertices = [
+            -0.5, -0.5, -0.5, // P - 0
+            0.5, -0.5, -0.5, // Q - 1
+            0.5, -0.5, 0.5, // R - 2
+            -0.5, -0.5, 0.5, // S - 3
+        ]
+
+        // this.indices = [
+        //     3,2,1,
+        //     1,0,3
+        // ]
+
+        this.indices = [
+            1,2,3,3,0,1
+		]
+		
+        this.normals = [
+			0,-1,0,
+			0,-1,0,
+			0,-1,0,
+			0,-1,0
+        ]
+
+        this.texCoords = [
+			0.25, 0.5,
+			0.5, 0.5,
+			0.5, 0.75,
+			0.25, 0.75
+        ]
+
+        this.initGLBuffers()
+	}
+
+	initTop() {
+		this.vertices = [
+            -0.5, 0.5, -0.5, // P - 0
+            0.5, 0.5, -0.5, // Q - 1
+            0.5, 0.5, 0.5, // R - 2
+            -0.5, 0.5, 0.5, // S - 3
+        ]
+
+        this.indices = [
+            3,2,1,
+            1,0,3
+        ]
+
+        // this.indices = [
+        //     1,2,3,3,0,1
+		// ]
+		
+        this.normals = [
+			0,1,0,
+			0,1,0,
+			0,1,0,
+			0,1,0
+        ]
+
+        this.texCoords = [
+			0.25, 0,
+			0.5, 0,
+			0.5, 0.25,
+			0.25, 0.25
+        ]
+
+        this.initGLBuffers()
+	}
+
+	initFront() {
+		this.vertices = [
+            -0.5, 0.5, 0.5, // P - 0
+            0.5, 0.5, 0.5, // Q - 1
+            0.5, -0.5, 0.5, // R - 2
+            -0.5, -0.5, 0.5, // S - 3
+        ]
+
+        this.indices = [
+            3,2,1,
+            1,0,3
+        ]
+
+        // this.indices = [
+        //     1,2,3,3,0,1
+		// ]
+		
+        this.normals = [
+			0,0,1,
+			0,0,1,
+			0,0,1,
+			0,0,1
+        ]
+
+        this.texCoords = [
+			0.25, 0.25,
+			0.5, 0.25,
+			0.5, 0.5,
+			0.25, 0.5
+        ]
+
+        this.initGLBuffers()
+	}
+
+	initFrontLeft() {
+		this.vertices = [
+            -0.5, 0.5, -0.5, // P - 0
+            -0.5, 0.5, 0.5, // Q - 1
+            -0.5, -0.5, 0.5, // R - 2
+            -0.5, -0.5, -0.5, // S - 3
+        ]
+
+        this.indices = [
+            3,2,1,
+            1,0,3
+        ]
+
+        // this.indices = [
+        //     1,2,3,3,0,1
+		// ]
+		
+        this.normals = [
+			-1,0,0,
+			-1,0,0,
+			-1,0,0,
+			-1,0,0
+        ]
+
+        this.texCoords = [
+			0, 0.25,
+			0.25, 0.25,
+			0.25, 0.5,
+			0, 0.5
+        ]
+
+        this.initGLBuffers()
+	}
+
+	initFrontRight() {
+		this.vertices = [
+            0.5, 0.5, 0.5, // P - 0
+            0.5, 0.5, -0.5, // Q - 1
+            0.5, -0.5, -0.5, // R - 2
+            0.5, -0.5, 0.5, // S - 3
+        ]
+
+        this.indices = [
+            3,2,1,
+            1,0,3
+        ]
+
+        // this.indices = [
+        //     1,2,3,3,0,1
+		// ]
+		
+        this.normals = [
+			1,0,0,
+			1,0,0,
+			1,0,0,
+			1,0,0
+        ]
+
+        this.texCoords = [
+			0.5, 0.25,
+			0.75, 0.25,
+			0.75, 0.5,
+			0.5, 0.5
+        ]
+
+        this.initGLBuffers()
+	}
+
+	initBack() {
+		this.vertices = [
+            0.5, 0.5, -0.5, // P - 0
+            -0.5, 0.5, -0.5, // Q - 1
+            -0.5, -0.5, -0.5, // R - 2
+            0.5, -0.5, -0.5, // S - 3
+        ]
+
+        this.indices = [
+            3,2,1,
+            1,0,3
+        ]
+
+        // this.indices = [
+        //     1,2,3,3,0,1
+		// ]
+		
+        this.normals = [
+			0,0,-1,
+			0,0,-1,
+			0,0,-1,
+			0,0,-1
+        ]
+
+        this.texCoords = [
+			0.75, 0.25,
+			1, 0.25,
+			1, 0.5,
+			0.75, 0.5
+        ]
+
+        this.initGLBuffers()
+	}
+
+	displayBase()
+	{
+		// Apply texture and set filtering
+        this.cubemap_texture.apply()
+		this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
+		
+		// Display cube faces
+		this.initBottom()
+		this.display()
+		
+		this.initTop()
+		this.display()
+
+		this.initFront()
+		this.display()
+
+		this.initFrontLeft()
+		this.display()
+
+		this.initFrontRight()
+		this.display()
+
+		this.initBack()
+		this.display()
+	}
+
 }
