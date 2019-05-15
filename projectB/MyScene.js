76074bff33f1bcc559047c962668085e1aab7c60
Gonçalo Oliveira <goncalo.oli@outlook.com>
2019-05-15 13:00:01 +0100

    Fixed terrain

--- projectB/MyScene.js
+++ projectB/MyScene.js
@@ -21,6 +21,8 @@ class MyScene extends CGFscene {
         this.enableTextures(true);
         this.setUpdatePeriod(50);
 
+        this.appearance = new CGFappearance(this);
+
         //Initialize scene objects
         this.axis = new CGFaxis(this);
         this.plane = new MyTerrain(this, 32);
@@ -79,16 +81,12 @@ class MyScene extends CGFscene {
         this.axis.display();
 
         //Apply default appearance
-        this.setDefaultAppearance();
-
+        // this.setDefaultAppearance();
+        this.appearance.apply();
+        
+        // ---- BEGIN Primitive drawing section
         this.displayScene();
 
-        // ---- BEGIN Primitive drawing section
-        this.pushMatrix();
-        this.rotate(-0.5*Math.PI, 1, 0, 0);
-        this.scale(60, 60, 1);
-        this.plane.display();
-        this.popMatrix();
         // ---- END Primitive drawing section
     }
 }
--- projectB/MyTerrain.js
+++ projectB/MyTerrain.js
@@ -7,12 +7,21 @@ class MyTerrain extends CGFobject {
         super(scene);
 
         this.plane = new Plane(scene, nrDivs);
-        this.plane_material = new CGFappearance(scene);
-        this.plane_material.loadTexture('images/terrain.jpg');
+        this.plane_material = new CGFtexture(scene, 'images/terrain.jpg');
+        
+        this.heightmap_material = new CGFtexture(scene, 'images/heightmap.jpg');
+
+        scene.appearance.setTexture(this.plane_material);
+        scene.appearance.setTextureWrap('REPEAT', 'REPEAT');
+        this.heightmap_material.bind(1);
+
+        this.plane_shader = new CGFshader(scene.gl, 'shaders/terrain.vert', 'shaders/terrain.frag');
+        this.plane_shader.setUniformsValues({uSampler: 1});
     }
 
     display() {
-        this.plane_material.apply();
+        this.scene.setActiveShader(this.plane_shader);
         this.plane.display();
+        this.scene.setActiveShader(this.scene.defaultShader);
     }
 }
